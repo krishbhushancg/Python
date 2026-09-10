d02fa1eb0090c43a7336891b4d95c7efb450ebef
krishbhushancg <krish.bhushan.cg@gmail.com>
2026-09-10 09:51:09 +0530

    loops added

--- Python_10/29.py
+++ Python_10/29.py
@@ -1,4 +1,4 @@
-for row in range(1, 6):
-    for column in range(1, row + 1):
-        print("*", end="")
+for row in range(1,6):
+    for column in range(1,row+1):
+        print(column, end="")
     print()
